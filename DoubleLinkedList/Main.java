7c85991c0085ee1290458fcf7ca4a1a7c0a443a6
Vanessa Rice <vanessa.rice@keyin.com>
2024-06-17 11:45:47 -0230

    Update Main.java

--- DoubleLinkedList/Main.java
+++ DoubleLinkedList/Main.java
@@ -6,7 +6,10 @@ public class Main {
         doublyLinkedList.insertDLL(1, 0);
         doublyLinkedList.insertDLL(2, 1);
         doublyLinkedList.insertDLL(3, 2);
-
+        doublyLinkedList.searchDLL(1);
         doublyLinkedList.traverseDLL();
+        doublyLinkedList.reverseTraverseDLL();
+        doublyLinkedList.deleteNodeDLL(1);
+        doublyLinkedList.deleteDLL();
     }
 }
